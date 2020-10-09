bad62985272ff9c70f95c0f9e3828f3f5d4ab417
Marketionist <marketionist@gmail.com>
2020-10-08 22:43:03 -0400

    Add messages

---
+++ telecom/messages/messages.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// #############################################################################
+
+let messages = {
+    CONFIRM_CLIENT_NAME: 'This is the Tyrell Corporation. Am I speaking with ' +
+        'Wile E. Coyote?',
+    CONFIRM_CLIENT_NUMBER: 'This is the Tyrell Corporation. I have an ' +
+        'important message for Wile E. Coyote. Is this the correct number?',
+
+    GREETINGS: ['Hello?', 'Hi', 'Hey'],
+    CONFIRMATIONS: ['Yes', 'Yep', 'Aha', 'This is he.', '👍'],
+
+    FINAL_MESSAGE: 'Your recent order includes an item that is being ' +
+        'recalled...'
+};
+
+module.exports = messages;
