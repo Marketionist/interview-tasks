6ec49f21ff5651d5538f13bf9a2aa97a243748d3
Marketionist <marketionist@gmail.com>
2021-11-22 22:09:33 -0500

    Update test script and verification

--- e2e-testing/login-feature/tests/specs/login.test.js
+++ e2e-testing/login-feature/tests/specs/login.test.js
@@ -1,12 +1,15 @@
 const LoginPage = require('../pageobjects/login.page.js');
 
 describe('Blockchain Wallet application', () => {
-    it('should login with valid credentials', async () => {
-        await LoginPage.open('/en/#/login');
-        await LoginPage.login('ccoaiqju@sharklasers.com', '5555TEST+');
+    it(
+        'should show verify device notification while logging in with valid credentials',
+        async () => {
+            await LoginPage.open('/en/#/login');
+            await LoginPage.login('ccoaiqju@sharklasers.com', '5555TEST+');
 
-        await expect(LoginPage.blockVerifyDevice).toBeExisting();
-    });
+            await expect(LoginPage.blockVerifyDevice).toBeDisplayed();
+        }
+    );
 });
 
 
