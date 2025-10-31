94cd5c4d299ebc3fc4feb9c048ed479c462052b9
Marketionist <marketionist@gmail.com>
2025-10-30 23:32:03 -0400

    Add projects to healthcare e2e config

--- healthcare/e2e-config.ts
+++ healthcare/e2e-config.ts
@@ -2,12 +2,15 @@ import { defineConfig, devices, } from '@playwright/test';
 
 export default defineConfig({
     /* Timeout is shared between all tests */
-    timeout: 240000,
+    timeout: 300000,
     retries: 0,
+    workers: 1,
     testDir: 'tests/e2e',
     testMatch: /.*(?<!fast|long)-e2e-spec\.ts/,
     /* Use list and json reporters while running on CI and only list locally */
-    reporter: process.env.CI ? [['list',], ['json', { outputFile: 'test-results/report.json' }],] : 'list',
+    reporter: process.env.CI ?
+        [['list',], ['json', { outputFile: 'test-results/report.json' }],] :
+        'list',
     use: {
         ...devices['Desktop Firefox'],
         headless: true,
@@ -17,4 +20,53 @@ export default defineConfig({
         video: 'retain-on-failure',
         screenshot: 'only-on-failure',
     },
+    projects: [
+        {
+            name: 'Happy path',
+            testMatch: /.*(?<!fast|long)-e2e-spec\.ts/,
+            retries: 0,
+        },
+        {
+            name: 'Log in',
+            testMatch: /global-setup\.ts/,
+            retries: 0,
+        },
+        {
+            name: 'Setup test data',
+            testMatch: /.*(?<!global-)setup\.ts/,
+            use: {
+                storageState: '.auth/user.json',
+            },
+            dependencies: ['Log in',],
+            retries: 0,
+        },
+        {
+            name: 'Fast',
+            testMatch: /.*fast-e2e-spec\.ts/,
+            use: {
+                storageState: '.auth/user.json',
+                /* An object containing additional HTTP headers to be sent with
+                every request */
+                // extraHTTPHeaders: {
+                //     'X-My-Header': 'value',
+                // },
+            },
+            dependencies: ['Setup test data',],
+            retries: 1,
+        },
+        {
+            name: 'Long',
+            testMatch: /.*long-e2e-spec\.ts/,
+            use: {
+                storageState: '.auth/user.json',
+                /* An object containing additional HTTP headers to be sent with
+                every request */
+                // extraHTTPHeaders: {
+                //     'X-My-Header': 'value',
+                // },
+            },
+            dependencies: ['Setup test data',],
+            retries: 2,
+        },
+    ],
 });
