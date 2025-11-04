8063a10c2bfbc05857c31cb22124b25ee5117d80
Marketionist <marketionist@gmail.com>
2025-11-03 22:24:16 -0500

    Add visual regression config for fintech

---
+++ fintech/visual-config.ts
@@ -0,0 +1,62 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+    /* Timeout is shared between all tests */
+    timeout: 60000,
+    retries: 0,
+    testDir: 'tests/visual',
+    testMatch: /.*(?<!fast|long)-visual-spec\.ts/,
+    expect: {
+        timeout: 10000,
+        toMatchSnapshot: {
+            maxDiffPixelRatio: 0.15,
+        },
+    },
+    use: {
+        ...devices['Desktop Firefox'],
+        headless: true,
+        viewport: { width: 1280, height: 720, },
+        actionTimeout: 20000,
+        ignoreHTTPSErrors: true,
+        video: 'off',
+        screenshot: 'off',
+    },
+    projects: [
+        {
+            name: 'Happy path',
+            testMatch: /.*(?<!fast|long)-visual-spec\.ts/,
+            retries: 0,
+        },
+        /* Test against desktop browsers */
+        {
+            name: 'Chromium',
+            use: { ...devices['Desktop Chrome'], },
+        },
+        {
+            name: 'Firefox',
+            use: { ...devices['Desktop Firefox'], },
+        },
+        {
+            name: 'Webkit',
+            use: { ...devices['Desktop Safari'], },
+        },
+        /* Test against mobile viewports */
+        {
+            name: 'Mobile Chrome',
+            use: { ...devices['Pixel 5'], },
+        },
+        {
+            name: 'Mobile Safari',
+            use: { ...devices['iPhone 12'], },
+        },
+        /* Test against branded browsers */
+        {
+            name: 'Google Chrome',
+            use: { ...devices['Desktop Chrome'], channel: 'chrome', }, // or 'chrome-beta'
+        },
+        {
+            name: 'Microsoft Edge',
+            use: { ...devices['Desktop Edge'], channel: 'msedge', }, // or 'msedge-dev'
+        },
+    ],
+});
